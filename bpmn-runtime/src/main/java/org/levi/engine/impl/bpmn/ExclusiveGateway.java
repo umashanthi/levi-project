a0bbbd06890ca90cbe70907a2ad682a1642493be
Eranda Sooriyabandara <eranda@eranda.org>
2011-01-12 14:14:56 +0530

    exclusive gateway with script enabled(partially) and parellel gateway with convergent parellel gateway working properly

--- bpmn-runtime/src/main/java/org/levi/engine/impl/bpmn/ExclusiveGateway.java
+++ bpmn-runtime/src/main/java/org/levi/engine/impl/bpmn/ExclusiveGateway.java
@@ -1,7 +1,10 @@
 package org.levi.engine.impl.bpmn;
 
+import org.apache.bsf.BSFException;
+import org.apache.bsf.BSFManager;
 import org.levi.samples.utils.UserInput;
 import org.omg.spec.bpmn.x20100524.model.TExclusiveGateway;
+import org.omg.spec.bpmn.x20100524.model.TExpression;
 import org.omg.spec.bpmn.x20100524.model.TSequenceFlow;
 
 import java.util.ArrayList;
@@ -18,22 +21,53 @@ public final class ExclusiveGateway extends Gateway {
     public List<TSequenceFlow> evaluate() {
         System.out.println("<Exclusive Gateway " + getName() + " Evaluating>");
         output.clear();
-        compare();
-        if (isDiverging()
-            || isConverging()
-            || isMixed()
-            || isUnspecified()) { // for exclusive gateway, all these are similar
-            if (outgoingSeqFlowSet.size() == 1) { // typical converging case
-                output.add(outgoingSeqFlowSet.get(0));
-            } else { // typical diverging case
-                // TODO evaluate!
-                int which = UserInput.read(outgoingSeqFlowSet.size());
-                output.add(outgoingSeqFlowSet.get(which));
+        //compare();
+        //if (isDiverging()
+        //    || isConverging()
+        //    || isMixed()
+        //    || isUnspecified()) { // for exclusive gateway, all these are similar
+        //    if (outgoingSeqFlowSet.size() == 1) { // typical converging case
+        //        output.add(outgoingSeqFlowSet.get(0));
+        //    } else { // typical diverging case
+        //        // TODO evaluate!
+        //        int which = UserInput.read(outgoingSeqFlowSet.size());
+        //        output.add(outgoingSeqFlowSet.get(which));
+        //    }
+        //    return output;
+        //} else {
+        //    throw new IllegalArgumentException("Invalid gateway direction: Exclusive Gateway");
+        //}
+
+        //Ignore the multiple input and multiple output Gateways since they are not recommended.
+        if(isDiverging() || (incomingSeqFlowSet.size()==1)) {
+            for(int i=0;i< outgoingSeqFlowSet.size();i++){
+                TExpression expression = outgoingSeqFlowSet.get(i).getConditionExpression();
+                String script = null;
+                if(expression != null){
+                    script = expression.newXMLStreamReader().getText();
+                    System.out.println(">>>>>=============>>>>>"+script);
+                } else{
+                    throw new IllegalArgumentException("No conditional expression found: Exclusive Gateway");
+                }
+
+                BSFManager manager = new BSFManager();
+                try {
+                    if((Boolean)manager.eval("groovy", "myScript", 0, 0, script)){
+                        //this evaluation may more complex
+                        output.add(outgoingSeqFlowSet.get(i));
+                        return output;
+                    }
+                } catch (BSFException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
             }
-            return output;
+        } else if(isConverging() || (outgoingSeqFlowSet.size()==1)) {
+            System.out.println("<<<<<=============<<<<<");
+            output.add(outgoingSeqFlowSet.get(0));
         } else {
             throw new IllegalArgumentException("Invalid gateway direction: Exclusive Gateway");
         }
+        return output;
     }
 
     private void compare() {
--- bpmn-runtime/src/main/java/org/levi/engine/impl/bpmn/ParallelGateway.java
+++ bpmn-runtime/src/main/java/org/levi/engine/impl/bpmn/ParallelGateway.java
@@ -17,13 +17,33 @@ public final class ParallelGateway extends Gateway {
     public List<TSequenceFlow> evaluate() {
         System.out.println("<Parallel Gateway " + getName() + " Evaluating>");
         output.clear();
-        if (compare()) {
-            for (int i = outgoingSeqFlowSet.size()-1; i >= 0; --i) {
+        //if (compare()) {
+        //    for (int i = outgoingSeqFlowSet.size()-1; i >= 0; --i) {
+        //        output.add(outgoingSeqFlowSet.get(i));
+        //    }
+        //} else {
+        //    //System.out.println(" not done...");
+        //}
+
+        if(isDiverging() || (incomingSeqFlowSet.size()==1)) {
+            for(int i=0;i< outgoingSeqFlowSet.size();i++){
+                System.out.println(">>>>>=============>>>>>");
                 output.add(outgoingSeqFlowSet.get(i));
             }
+        } else if(isConverging() || (outgoingSeqFlowSet.size()==1)) {
+            System.out.println("<<<<<=============<<<<<");
+            if(incomingSeqFlowSet.size() == 1){
+                output.add(outgoingSeqFlowSet.get(0));
+            } else {
+                if(incomingTokens.size() == incomingSeqFlowSet.size()) {
+                    output.add(outgoingSeqFlowSet.get(0));
+                }
+            }
+
         } else {
-            //System.out.println(" not done...");
+            throw new IllegalArgumentException("Invalid gateway direction: Exclusive Gateway");
         }
+
         return output;
     }
 
