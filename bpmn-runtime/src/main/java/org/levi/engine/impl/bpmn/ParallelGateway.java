f7195fbc461a2ff903157ac27e7f6d6d025bd26e
Eranda Sooriyabandara <eranda@eranda.org>
2011-01-31 14:55:44 +0530

    eranda's exception fixed

--- bpmn-runtime/src/main/java/org/levi/engine/impl/bpmn/ParallelGateway.java
+++ bpmn-runtime/src/main/java/org/levi/engine/impl/bpmn/ParallelGateway.java
@@ -18,30 +18,8 @@ public final class ParallelGateway extends Gateway {
         System.out.println("<Parallel Gateway " + getName() + " Evaluating>");
         output.clear();
         if (compare()) {
-        //    for (int i = outgoingSeqFlowSet.size()-1; i >= 0; --i) {
-        //        output.add(outgoingSeqFlowSet.get(i));
-        //    }
-        //} else {
-        //    //System.out.println(" not done...");
-        //}
-
-            if(isDiverging() || (incomingSeqFlowSet.size()==1)) {
-                for(int i=0;i< outgoingSeqFlowSet.size();i++){
-                    System.out.println(">>>>>=============>>>>>");
-                    output.add(outgoingSeqFlowSet.get(i));
-                }
-            } else if(isConverging() || (outgoingSeqFlowSet.size()==1)) {
-                System.out.println("<<<<<=============<<<<<");
-                if(incomingSeqFlowSet.size() == 1){
-                    output.add(outgoingSeqFlowSet.get(0));
-                } else {
-                    if(incomingTokens.size() == incomingSeqFlowSet.size()) {
-                        output.add(outgoingSeqFlowSet.get(0));
-                    }
-                }
-
-            } else {
-                System.out.println(" not done...");
+            for (int i = 0; i < outgoingSeqFlowSet.size(); i++) {
+                output.add(outgoingSeqFlowSet.get(i));
             }
         }
 
@@ -72,7 +50,8 @@ public final class ParallelGateway extends Gateway {
         return result;
     }
 
-    @Override public String toString() {
+    @Override
+    public String toString() {
         String out = "{";
         out += "ParallelGateway: " + getId() + "}";
         return out;
