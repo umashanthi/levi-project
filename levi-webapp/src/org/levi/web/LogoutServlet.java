42d031a8c7f541ae6cab1b71b8ad3f9a077cd0c6
umashanthi <umashanthip@gmail.com>
2011-03-29 14:57:29 +0530

    Added login,logout& fileuploading functionality to levi-webapp module

---
+++ levi-webapp/src/org/levi/web/LogoutServlet.java
@@ -0,0 +1,21 @@
+package org.levi.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
