54e55863ab6608597942f0f329bc5d30605dcb93
Umashanthi <umashanthip@gmail.com>
2011-05-06 11:45:05 +0530

    Adding capability to show generated process diagrams for deployed processes

--- levi-webapp/src/main/java/org/levi/web/FileUploadServlet.java
+++ levi-webapp/src/main/java/org/levi/web/FileUploadServlet.java
@@ -45,13 +45,20 @@ public class FileUploadServlet extends HttpServlet {
 
                             if (fileItem.getSize() > 0) { //uploaded file item
                                 String fileName = fileItem.getName();
-                                String dirName = "LeviEngine/";
+                                String dirName = "../webapps/levi/LeviEngine/";
+                                File leviDir = new File(dirName);
+                                if (!leviDir.exists()) {
+                                    leviDir.mkdir();
+                                }
                                 File saveTo = new File(dirName + fileName);
+                                File dir = new File(dirName);
+                                String dirPath = dir.getAbsolutePath();
                                 try {
                                     fileItem.write(saveTo);   // stores the uploaded business archive (lar) to a location inside the server
                                     ProcessEngine engine = (ProcessEngine) request.getSession().getAttribute("processEngine");
                                     String larPath = dirName + fileName;
                                     engine.deploy(larPath);   // deploys the stored business archive (lar)
+
                                     response.sendRedirect("index.jsp?isUploadSuccess=true");
                                 } catch (Exception e) {
                                     response.sendRedirect("index.jsp?isUploadSuccess=false");
