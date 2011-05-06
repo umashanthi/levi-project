f8178bc3a717121f758c56661a14e4ea30e16762
Umashanthi <umashanthip@gmail.com>
2011-05-06 11:43:40 +0530

    Changes related to user management

--- bpmn-runtime/src/main/java/org/levi/engine/db/DBManager.java
+++ bpmn-runtime/src/main/java/org/levi/engine/db/DBManager.java
@@ -10,10 +10,6 @@ import org.levi.engine.impl.identity.Permission;
 
 public interface DBManager {
 
-    void createConnection();
-
-    void closeConnection();
-
     /**
      * This method saves a User to the database; if the User already exists, it updates the attributes
      *
@@ -23,28 +19,35 @@ public interface DBManager {
 
     void saveGroup(Group group);
 
-    void savePermission(Permission permission);
-
     User getUser(String userId);
 
     Group getGroup(String groupId);
 
-    Permission getPermission(String permissionId);
-
     void addUserToGroup(String userId, String groupId);
-
-    void addPermissionToRole(String roleId, String permissionId);
 
     void deleteUser(String userId);        //?? Only ids or the User object?
 
     void deleteGroup(String groupId);
 
-    void deletePermission(String permissionId);
-
     void removeUserFromGroup(String userId,
                              String groupId); //?? User& Group objects or simple the ids?
 
-    void removePermissionForRole(String roleId, String permissionId);
+    /* methods to be added
+        saveTask()
+        deleteTask()
+        saveProcess()
+        deleteProcess()
+        updateProcess()
+        updateTask()
+        getUserTaskList(String userId)
+        getGroupTaskList(String groupId)
+        getRunningProcessesList()
+        getAssigneeForTask(String taskId)
+        getActiveTasks()
+        getActiveTasks(String processId)
+
+
+     */
 
 
 }
