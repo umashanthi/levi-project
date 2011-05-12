c534fd9195d5c2b20d41dbdadead87a5b6fe2190
Umashanthi <umashanthip@gmail.com>
2011-05-12 14:23:53 +0530

    removing annoation in SubmitUserTaskFormServlet

--- levi-webapp/src/main/java/org/levi/web/SubmitUserTaskFormServlet.java
+++ levi-webapp/src/main/java/org/levi/web/SubmitUserTaskFormServlet.java
@@ -1,7 +1,6 @@
 package org.levi.web;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +13,7 @@ import java.io.IOException;
  * Time: 4:05 PM
  * To change this template use File | Settings | File Templates.
  */
-@WebServlet(name = "SubmitUserTaskFormServlet")
+
 public class SubmitUserTaskFormServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
               doGet(request, response);
