ab4bbd5f0a329c098f5c2861a8e6ee2aca75f9eb
Ishan Jayawardena <udeshike@gmail.com>
2011-05-13 11:04:24 +0530

    modified beans

--- bpmn-runtime/src/main/java/org/levi/engine/persistence/hibernate/process/DeploymentBean.java
+++ bpmn-runtime/src/main/java/org/levi/engine/persistence/hibernate/process/DeploymentBean.java
@@ -1,6 +1,9 @@
 package org.levi.engine.persistence.hibernate.process;
 
+import org.levi.engine.identity.User;
+
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Ishan Jayawardena.
@@ -10,7 +13,14 @@ public class DeploymentBean {
     private String extractPath;
     private String processDefinitionPath;
     private String diagramPath;
+    private User deployedUser;
     private Date deploymentTime;
+    private String businessArchiveName;
+    private String businessArchiveLocation;
+    private List<String> businessArchiveContents;// should list all the contents eg:forms etc
+    private User undeployedUser;
+    private Date undeployedTime;
+    private boolean isUndeployed; // i.e. undeployed
 
     public String getDefinitionsId() {
         return definitionsId;
@@ -44,11 +54,67 @@ public class DeploymentBean {
         this.diagramPath = diagramPath;
     }
 
+    public User getDeployedUser() {
+        return deployedUser;
+    }
+
+    public void setDeployedUser(User deployedUser) {
+        this.deployedUser = deployedUser;
+    }
+
     public Date getDeploymentTime() {
         return deploymentTime;
     }
 
     public void setDeploymentTime(Date deploymentTime) {
         this.deploymentTime = deploymentTime;
+    }
+
+    public String getBusinessArchiveName() {
+        return businessArchiveName;
+    }
+
+    public void setBusinessArchiveName(String businessArchiveName) {
+        this.businessArchiveName = businessArchiveName;
+    }
+
+    public String getBusinessArchiveLocation() {
+        return businessArchiveLocation;
+    }
+
+    public void setBusinessArchiveLocation(String businessArchiveLocation) {
+        this.businessArchiveLocation = businessArchiveLocation;
+    }
+
+    public List<String> getBusinessArchiveContents() {
+        return businessArchiveContents;
+    }
+
+    public void setBusinessArchiveContents(List<String> businessArchiveContents) {
+        this.businessArchiveContents = businessArchiveContents;
+    }
+
+    public User getUndeployedUser() {
+        return undeployedUser;
+    }
+
+    public void setUndeployedUser(User undeployedUser) {
+        this.undeployedUser = undeployedUser;
+    }
+
+    public Date getUndeployedTime() {
+        return undeployedTime;
+    }
+
+    public void setUndeployedTime(Date undeployedTime) {
+        this.undeployedTime = undeployedTime;
+    }
+
+    public boolean isUndeployed() {
+        return isUndeployed;
+    }
+
+    public void setUndeployed(boolean undeployed) {
+        isUndeployed = undeployed;
     }
 }
--- bpmn-runtime/src/main/java/org/levi/engine/persistence/hibernate/process/ProcessBean.java
+++ bpmn-runtime/src/main/java/org/levi/engine/persistence/hibernate/process/ProcessBean.java
@@ -1,8 +1,5 @@
 package org.levi.engine.persistence.hibernate.process;
 
-import org.levi.engine.identity.User;
-
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -18,15 +15,7 @@ import java.util.Map;
 
 public class ProcessBean {
     private String id; //primary key
-    private String processDefName;
-    private String businessArchiveName;
-    private String businessArchiveLocation;
-    private List<String> businessArchiveContents;// should list all the contents eg:forms etc
-    private User deployedUser;
-    private User undeployedUser;
-    private Date deployedTime;
-    private boolean isDeleted; // i.e. undeployed
-    private Date undeployedTime;
+    private String processDefId;
     private boolean isRunning;
     private Map<String, Object> variables;
     private List<String> runningTaskIds;
@@ -40,76 +29,12 @@ public class ProcessBean {
         this.id = id;
     }
 
-    public String getProcessDefName() {
-        return processDefName;
+    public String getProcessDefId() {
+        return processDefId;
     }
 
-    public void setProcessDefName(String processDefName) {
-        this.processDefName = processDefName;
-    }
-
-    public String getBusinessArchiveName() {
-        return businessArchiveName;
-    }
-
-    public void setBusinessArchiveName(String businessArchiveName) {
-        this.businessArchiveName = businessArchiveName;
-    }
-
-    public String getBusinessArchiveLocation() {
-        return businessArchiveLocation;
-    }
-
-    public void setBusinessArchiveLocation(String businessArchiveLocation) {
-        this.businessArchiveLocation = businessArchiveLocation;
-    }
-
-    public List<String> getBusinessArchiveContents() {
-        return businessArchiveContents;
-    }
-
-    public void setBusinessArchiveContents(List<String> businessArchiveContents) {
-        this.businessArchiveContents = businessArchiveContents;
-    }
-
-    public User getDeployedUser() {
-        return deployedUser;
-    }
-
-    public void setDeployedUser(User deployedUser) {
-        this.deployedUser = deployedUser;
-    }
-
-    public User getUndeployedUser() {
-        return undeployedUser;
-    }
-
-    public void setUndeployedUser(User undeployedUser) {
-        this.undeployedUser = undeployedUser;
-    }
-
-    public Date getDeployedTime() {
-        return deployedTime;
-    }
-
-    public void setDeployedTime(Date deployedTime) {
-        this.deployedTime = deployedTime;
-    }
-
-    public boolean isDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-    }
-
-    public Date getUndeployedTime() {
-        return undeployedTime;
-    }
-
-    public void setUndeployedTime(Date undeployedTime) {
-        this.undeployedTime = undeployedTime;
+    public void setProcessDefId(String processDefId) {
+        this.processDefId = processDefId;
     }
 
     public boolean isRunning() {
@@ -142,25 +67,5 @@ public class ProcessBean {
 
     public void setCompletedTaskIds(List<String> completedTaskIds) {
         this.completedTaskIds = completedTaskIds;
-    }
-
-    @Override
-    public String toString() {
-        return "ProcessBean{" +
-                "id='" + id + '\'' +
-                ", processDefName='" + processDefName + '\'' +
-                ", businessArchiveName='" + businessArchiveName + '\'' +
-                ", businessArchiveLocation='" + businessArchiveLocation + '\'' +
-                ", businessArchiveContents=" + businessArchiveContents +
-                ", deployedUser=" + deployedUser +
-                ", undeployedUser=" + undeployedUser +
-                ", deployedTime=" + deployedTime +
-                ", isDeleted=" + isDeleted +
-                ", undeployedTime=" + undeployedTime +
-                ", isRunning=" + isRunning +
-                ", variables=" + variables +
-                ", runningTaskIds=" + runningTaskIds +
-                ", completedTaskIds=" + completedTaskIds +
-                '}';
     }
 }
